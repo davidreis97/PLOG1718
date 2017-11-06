26d3545bb635e0d976ad3777a652633230918e4e
David Reis <david.alex.reis.97@gmail.com>
2017-11-06 12:33:42 +0000

    Added piece counter, win detection, improved play loop and initial call

--- GoRoGo/src/file.pl
+++ GoRoGo/src/file.pl
@@ -1,10 +1,10 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:3; tab-width:8; -*- */
 
-initBoard([[1,2,1,0,0],
-           [3,0,3,0,0],
-           [0,2,0,0,1],
-           [2,1,2,0,0],
-           [0,2,1,3,0]]).
+initBoard([[0,0,0,0,0],
+           [0,0,0,0,0],
+           [0,0,0,0,0],
+           [0,0,0,0,0],
+           [0,0,0,0,0]]).
 
 /*
   0- Empty Space
@@ -13,27 +13,108 @@ initBoard([[1,2,1,0,0],
   3- Mixed Piece
 */
 
-/*
-        TODO:
-        - No need to check the whole board for removed pieces, just check the 4 surrounding positions. (Helps with UI as well)
-*/
+pieceAllowed(1,1,WPieces,_,_,_) :-
+        WPieces > 0.
+pieceAllowed(1,3,_,WMixed,_,_) :-
+        WMixed > 0.
 
-playerInput(0,PlayerNo,Est,Linha,Coluna,Tipo). /* Player controlled move decision */
+pieceAllowed(2,2,_,_,BPieces,_) :-
+        BPieces > 0.
+pieceAllowed(2,3,_,_,_,BMixed) :-
+        BMixed > 0.
 
-playerInput(1,PlayerNo,Est,Linha,Coluna,Tipo) :- /* CPU controlled move decision */
+
+playerInput(0,PlayerNo,Est,Linha,Coluna,Tipo,WPieces,WMixed,BPieces,BMixed) :- /* Player controlled move decision */
+        repeat, 
+                print('Type of piece: '),
+                read(Tipo), 
+                pieceAllowed(PlayerNo,Tipo,WPieces,WMixed,BPieces,BMixed), !,
+        repeat,
+                print('Line: '),
+                read(Linha),
+                Linha > -1,
+                Linha < 5,
+                print('Column: '),
+                read(Coluna),
+                Coluna > -1,
+                Coluna < 5,
+                getPos(Linha,Coluna,Est,0),
+                \+ surrounded(Linha,Coluna,Est), !.
+                
+        
+playerInput(1,PlayerNo,Est,Linha,Coluna,Tipo,WPieces,WMixed,BPieces,BMixed) :- /* CPU controlled move decision, for now it's random */
+        use_module(library(random)),
+        repeat,
+                random(1,4,Tipo),
+                pieceAllowed(PlayerNo,Tipo,WPieces,WMixed,BPieces,BMixed), !,
+        repeat,
+                random(0,5,Linha),
+                random(0,5,Coluna),
+                getPos(Linha,Coluna,Est,0),
+                \+ surrounded(Linha,Coluna,Est), !.
+
+countPieces(1,1,WPieces,WMixed,BPieces,BMixed,WPieces1,WMixed1,BPieces1,BMixed1) :- 
+        WPieces1 is WPieces - 1,
+        WMixed1 is WMixed,
+        BPieces1 is BPieces,
+        BMixed1 is BMixed.
+
+countPieces(1,3,WPieces,WMixed,BPieces,BMixed,WPieces1,WMixed1,BPieces1,BMixed1) :- 
+        WPieces1 is WPieces,
+        WMixed1 is WMixed - 1,
+        BPieces1 is BPieces,
+        BMixed1 is BMixed.
+
+countPieces(2,2,WPieces,WMixed,BPieces,BMixed,WPieces1,WMixed1,BPieces1,BMixed1) :- 
+        WPieces1 is WPieces,
+        WMixed1 is WMixed,
+        BPieces1 is BPieces - 1,
+        BMixed1 is BMixed.
+
+countPieces(2,3,WPieces,WMixed,BPieces,BMixed,WPieces1,WMixed1,BPieces1,BMixed1) :- 
+        WPieces1 is WPieces,
+        WMixed1 is WMixed,
+        BPieces1 is BPieces,
+        BMixed1 is BMixed - 1.
         
 
-play(Player1,Player2,Board,EatenPieces) :- /*0 if Player controlled, 1 if AI*/
-        playerInput(Player1,1,Board,Linha1,Coluna1,Tipo1),
+play(Player1,Player2) :-
+        initBoard(Board),
+        play(Player1,Player2,Board,0,5,3,5,3).
+        
+play(Player1,Player2,Board,EatenPieces,WPieces,WMixed,BPieces,BMixed) :- /*0 if Player controlled, 1 if AI*/
+        playerInput(Player1,1,Board,Linha1,Coluna1,Tipo1,WPieces,WMixed,BPieces,BMixed),
         movePlayer(Linha1,Coluna1, Board, Board1, Tipo1), /* Assumes success in movePlayer */
+        countPieces(1,Tipo1,WPieces,WMixed,BPieces,BMixed,WPieces1,WMixed1,BPieces1,BMixed1),
         removeEaten(0,0,Board1,Board2,EatenPieces,EatenPieces1),
         printBoard(Board2),
-        playerInput(Player2,1,Board2,Linha2,Coluna2,Tipo2),
+        playerInput(Player2,2,Board2,Linha2,Coluna2,Tipo2,WPieces,WMixed,BPieces,BMixed),
         movePlayer(Linha2,Coluna2, Board2, Board3, Tipo2), /* Assumes success in movePlayer */
+        countPieces(2,Tipo2,WPieces1,WMixed1,BPieces1,BMixed1,WPieces2,WMixed2,BPieces2,BMixed2),
         removeEaten(0,0,Board3,Board4,EatenPieces1,EatenPieces2),
         printBoard(Board4),
-        play(Player1,Player2,Board4,EatenPieces2). /* Check victory conditions */
+        (endGame(Board4,WPieces2,WMixed2,BPieces2,BMixed2, 0) ; play(Player1,Player2,Board4,EatenPieces2,WPieces2,WMixed2,BPieces2,BMixed2)). /* Check victory conditions */
 
+endGame([],0,0,0,0,Count) :-
+        Count > 0,
+        print('Whites win!').
+endGame([],0,0,0,0,Count) :-
+        Count < 0,
+        print('Blacks win!').
+endGame([],0,0,0,0,0) :-
+        print('Tie!').
+endGame([[]|Xs],0,0,0,0,Count) :-
+        endGame(Xs,0,0,0,0, Count).
+endGame([[1|Xs]|Xss],0,0,0,0, Count) :- 
+        !,
+        NewCount is Count+1,
+        endGame([Xs|Xss],0,0,0,0, NewCount).
+endGame([[2|Xs]|Xss],0,0,0,0, Count) :- 
+        !,
+        NewCount is Count-1,
+        endGame([Xs|Xss],0,0,0,0, NewCount).
+endGame([[_|Xs]|Xss],0,0,0,0, Count) :-
+        endGame([Xs|Xss],0,0,0,0, Count).
 
 printBoardLine([]) :- 
         print('|').
@@ -51,10 +132,10 @@ printBoardLine([2|R]) :-
         printBoardLine(R).
 printBoardLine([3|R]) :-
         print('|'), 
-        print('M'),
+        print('H'),
         printBoardLine(R).
 
-printBoard([]) :- print('-----------').
+printBoard([]) :- print('-----------'), nl.
 printBoard([L|R]) :-
         print('-----------'),
         print('\n'),
@@ -79,7 +160,7 @@ removePiece(Linha,Coluna,[L|R],[L|R1]) :-
         LinhaNova is Linha-1,
         removePiece(LinhaNova,Coluna,R,R1).
 
-movePlayer(0,0,[[0|LR]|R],[[Tipo|LR]|R], Tipo). /* Add check if surrounded */
+movePlayer(0,0,[[0|LR]|R],[[Tipo|LR]|R], Tipo). /* Assumes not surrounded */
 movePlayer(0, Coluna, [[L|LR]|R], [[L|LR1]|R1], Tipo) :-
         Coluna \= 0,
         ColunaNova is Coluna-1,
@@ -136,6 +217,7 @@ removeEaten(Linha,Coluna,Old,NewNew,EatenPiecesOld,EatenPiecesNew) :-
         removePiece(Linha,Coluna,Old,New),
         ColunaNova is Coluna+1,
         EatenPiecesInc is EatenPiecesOld + 1,
+        print('Piece Eaten'),
         removeEaten(Linha,ColunaNova,New,NewNew,EatenPiecesInc,EatenPiecesNew).
 removeEaten(Linha,Coluna,Old,New,EatenPiecesOld,EatenPiecesNew) :- 
         Linha < 5,
